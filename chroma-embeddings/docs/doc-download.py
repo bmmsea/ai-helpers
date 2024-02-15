fce006d370e6cc12e91e5036f4fb4c9035e277a7
bmmsea <bmmsea@gmail.com>
2024-02-15 14:59:44 -0800

    Refactored document download script to use a custom URL loader and added file saving functionality.

--- chroma-embeddings/docs/doc-download.py
+++ chroma-embeddings/docs/doc-download.py
@@ -1,19 +1,32 @@
-import os
-import requests
-from urllib.parse import urlparse
 
-def download_documents():
-    with open('input-urls.txt', 'r') as file:
+from urllib.parse import urlparse
+from langchain_community.document_loaders import UnstructuredURLLoader
+
+input_file = 'input-urls.txt'
+urls = []
+
+def lang_load():
+    loader = UnstructuredURLLoader(urls=urls)
+    data = loader.load()
+
+def save_file(filepath, contents):
+    with open(filename, 'wb') as output_file:
+        output_file.write(response.content)
+
+def load_input_urls(filename):
+    with open(filename, 'r') as file:
         urls = file.readlines()
 
     for url in urls:
         url = url.strip()
-        response = requests.get(url)
+        
         parsed_url = urlparse(url)
         filename = "docs/raw/" + (parsed_url.netloc + parsed_url.path).replace('/', '_')
+        
+        print(filename)
+        
 
-        with open(filename, 'wb') as output_file:
-            output_file.write(response.content)
+
 
 if __name__ == "__main__":
-    download_documents()
+    load_input_urls(input_file)
