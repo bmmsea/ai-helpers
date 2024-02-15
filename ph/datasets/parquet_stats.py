91ae3e786f519a6219583c4c837c015f7221223a
bmmsea <bmmsea@gmail.com>
2024-02-14 16:06:37 -0800

    Filter parquet file data to include only the year 2023 and specific columns.

--- ph/datasets/parquet_stats.py
+++ ph/datasets/parquet_stats.py
@@ -3,12 +3,14 @@ from datetime import datetime
 import pandas as pd
 import csv, gzip, re, sys, time, tqdm
 
-parquet_file = 'big.parquet'
+stat_year = '2023'
+parquet_file = f"big.parquet/year={stat_year}/*"
 start_time = time.time()
 
 # Load the parquet file into a pandas DataFrame
 df = pd.read_parquet(parquet_file)
 
 # Filter the DataFrame to only include data from the year 2023
-df = df[df['year'] == 2023]
+#df = df[df['year'] == 2023]
 
+columns = ['iframe', 'main_auth_thumbnail', 'title', 'tags', 'categories', 'performers', 'length', 'views', 'likes', 'dislikes', 'date', 'year']
